01c8c06f6d13b7d9acfc53ee429ca8d8878787ff
Pholisa-Fatyela <pholisa@projectcodex.co>
2016-09-21 16:37:25 +0200

    todays work

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
+  http_basic_authenticate_with name: "pholisa", password: "secret", except: [:index, :show]
 
   # GET /topics
   # GET /topics.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :topics
-  resources :topics
   resources :topics do
-    member do 
+    resources :comments
+  end
+  resources :topics do
+    member do
       post 'upvote'
     end
   end
